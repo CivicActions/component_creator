b80158833347ffe5d05c7191c960e84f88b59dd0
Tom Camp <tomcamp@protonmail.ch>
2022-12-30 15:35:50 -0500

    Feature/controls (#5)
    * Adding Catalog update.
    
    * Adding catalog delete.
    
    * Updating Catalog CRUD.'
    
    * Adding catalog control view.

--- app/catalogs/routes.py
+++ app/catalogs/routes.py
@@ -5,7 +5,7 @@ from jinja2 import TemplateNotFound
 from werkzeug.utils import secure_filename
 
 from app.catalogs import bp
-from app.catalogs.catalog_forms import CatalogForm
+from app.catalogs.forms import CatalogForm, UpdateCatalogForm
 from app.extensions import db
 from app.models.components import Catalog
 from app.oscal.catalog import CatalogModel
@@ -90,6 +90,7 @@ def catalog_add():
                 except db.SQLAlchemyError as exc:
                     error = f"Catalog {title} already exists: {exc}"
                 else:
+                    flash(f"Catalog {title} created.", "message")
                     return redirect(
                         url_for("catalogs.catalog_view", catalog_id=catalog.id)
                     )
@@ -106,15 +107,74 @@ def catalog_add():
 
 @bp.route("/<int:catalog_id>", methods=["GET"])
 def catalog_view(catalog_id: int):
-    catalog_data = db.session.execute(
-        db.select(Catalog).filter_by(id=catalog_id)
-    ).first()
-    catalog = CatalogModel.from_json(catalog_data[0].filename)
+    catalog_data = Catalog.query.get_or_404(catalog_id)
+    catalog = CatalogModel.from_json(catalog_data.filename)
     metadata = catalog.metadata
     groups = catalog.get_groups()
     return render_template(
-        "catalog.html",
-        metadata=metadata,
-        groups=groups,
+        "catalog.html", metadata=metadata, groups=groups, catalog=catalog_data
+    )
+
+
+@bp.route("/<int:catalog_id>/update", methods=["GET", "POST"])
+def catalog_update(catalog_id: int):
+    form = UpdateCatalogForm()
+    catalog = Catalog.query.get_or_404(catalog_id)
+
+    if request.method == "POST":
+        error = None
+        if form.validate_on_submit():
+            catalog.title = request.form["name"]
+            catalog.description = request.form["description"]
+
+            try:
+                db.session.add(catalog)
+                db.session.commit()
+            except db.IntegrityError:
+                error = f"Catalog {catalog_id} update failed."
+            else:
+                flash(f"Catalog {catalog.title} has been updated.")
+                return redirect(url_for("catalogs.catalog_view", catalog_id=catalog_id))
+
+        flash(error)
+
+    form.name.data = catalog.title
+    form.description.data = catalog.description
+    return render_template(
+        "catalog_update_form.html",
+        form=form,
+        title="Update Catalog",
+        catalog=catalog,
+    )
+
+
+@bp.route("/<int:catalog_id>/delete", methods=["GET"])
+def catalog_delete(catalog_id: int):
+    catalog = Catalog.query.get_or_404(catalog_id)
+    db.session.delete(catalog)
+    db.session.commit()
+    Path(catalog.filename).unlink()
+    flash(f"Catalog {catalog.title} has been deleted.")
+    return redirect((url_for("catalogs.catalogs_list")))
+
+
+@bp.route("/<int:catalog_id>/control/<string:control_id>", methods=["GET"])
+def control_view(catalog_id: int, control_id: str):
+    catalog_data = Catalog.query.get_or_404(catalog_id)
+    catalog = CatalogModel.from_json(catalog_data.filename)
+    group = catalog.get_group(control_id)
+    control = catalog.get_control(control_id)
+    guidance = control.guidance
+    parameters = control.parameters
+    statement = control.statement
+    statements = replace_odps(statement, parameters)
+    links = get_control_links(control.links, catalog.back_matter)
+    return render_template(
+        "control.html",
+        control=control,
+        links=links,
+        statement=statements,
         catalog=catalog_data,
+        guidance=guidance,
+        group=group,
     )
